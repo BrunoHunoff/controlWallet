ada4fa995098bb3a754bd689a7add1ef8393f93c
diegoromal <diego@qosit.com.br>
2024-06-11 19:39:38 -0300

    Adiciona classe abstrata Pessoa

---
+++ src/models/Pessoa.java
@@ -0,0 +1,36 @@
+package models;
+
+public abstract class Pessoa {
+
+    private String nome;
+    private int idade;
+
+    public Pessoa(String nome, int idade) {
+        this.nome = nome;
+        this.idade = idade;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+
+    @Override
+    public String toString() {
+        return "\nNome: " + nome + 
+               "\nIdade: " + idade;
+    }   
+
+    
+}
