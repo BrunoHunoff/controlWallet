01622fc5e30176be687de2eb0500c25cd1761486
diegoromal <diego@qosit.com.br>
2024-06-11 21:08:31 -0300

    Adiciona classe Main para testes

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,11 @@
-import java.util.Scanner;
-
 import controllers.GerenciarUsuario;
-
+import helpers.Console;
+import models.Usuario;
 public class Main {
     public static void main(String[] args) {
-        GerenciarUsuario userManager = new GerenciarUsuario();
-        Scanner scanner = new Scanner(System.in);
-        int option;
+        GerenciarUsuario gerenciarUsuario = new GerenciarUsuario();
+        
+        int opcao;
 
         do {
             System.out.println("1. Criar Usuário");
@@ -14,45 +13,45 @@ public class Main {
             System.out.println("3. Atualizar Usuário");
             System.out.println("4. Deletar Usuário");
             System.out.println("5. Sair");
-            System.out.print("Escolha uma opção: ");
-            option = scanner.nextInt();
-            scanner.nextLine();  // Consume newline
+            opcao = Console.lerInt("Escolha uma opção: ");
+            
 
-            switch (option) {
+            switch (opcao) {
                 case 1:
-                    System.out.print("Digite o username: ");
-                    String username = scanner.nextLine();
-                    System.out.print("Digite a senha: ");
-                    String password = scanner.nextLine();
-                    userManager.createUser(username, password);
+                    String nomeCompleto = Console.lerString("Digite o nome completo do usuário: ");
+                    String nomeUsuario = Console.lerString("Digite o nome de usuario: ");
+                    String senha = Console.lerString("Digite a senha: ");
+
+                    gerenciarUsuario.criarUsuario(nomeCompleto, nomeUsuario, senha);
                     break;
+
                 case 2:
-                    for (User user : userManager.readUsers()) {
-                        System.out.println(user);
+                    for (Usuario usuario : gerenciarUsuario.listarTodos()) {
+                        System.out.println(usuario);
                     }
                     break;
+
                 case 3:
-                    System.out.print("Digite o ID do usuário: ");
-                    String idToUpdate = scanner.nextLine();
-                    System.out.print("Digite o novo username: ");
-                    String newUsername = scanner.nextLine();
-                    System.out.print("Digite a nova senha: ");
-                    String newPassword = scanner.nextLine();
-                    userManager.updateUser(idToUpdate, newUsername, newPassword);
+                    String idUsuarioAtualizar = Console.lerString("Digite o ID do usuário: ");
+                    String novoNomeCompleto = Console.lerString("Digite o novo nome completo do usuário: ");
+                    String novoNomeUsuario = Console.lerString("Digite o novo nome de usuario: ");
+                    String novaSenha = Console.lerString("Digite a nova senha: ");
+
+                    gerenciarUsuario.atualizarUsuario(idUsuarioAtualizar, novoNomeCompleto, novoNomeUsuario, novaSenha);
                     break;
+
                 case 4:
-                    System.out.print("Digite o ID do usuário: ");
-                    String idToDelete = scanner.nextLine();
-                    userManager.deleteUser(idToDelete);
+                    String idUsuarioDeletar = Console.lerString("Digite o ID do usuário: ");
+                    
+                    gerenciarUsuario.deletarUsuario(idUsuarioDeletar);
                     break;
+
                 case 5:
-                    System.out.println("Saindo...");
+                    System.out.println("Finalizando o sistema...");
                     break;
                 default:
                     System.out.println("Opção inválida!");
             }
-        } while (option != 5);
-
-        scanner.close();
+        } while (opcao != 5);
     }
 }
