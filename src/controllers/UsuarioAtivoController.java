8f6dd3bdd52b4adb37cf3a5f6e3ba4db80d48dbc
PedroSirigatti <pedrosirigatti@gmail.com>
2024-06-20 15:12:09 -0300

    Classe 'UsuarioAtivoController' adicionada

--- src/controllers/UsuarioAtivoController.java
+++ src/controllers/UsuarioAtivoController.java
@@ -49,7 +49,7 @@ public class UsuarioAtivoController {
     }
 
     public ArrayList<UUID> listarAtivosDoUsuario(String idUsuario) {
-        Usuario usuario = getUsuarioId(idUsuario);
+        Usuario usuario = getUsuarioId(idUsuario);""
         if (usuario != null) {
             return usuario.getCarteira();
         }
