056c653c122316a60f5cd39ff7473842e031ec30
diegoromal <diego@qosit.com.br>
2024-06-23 14:09:59 -0300

    feat - Opção remover ação

--- src/views/MenuAcao.java
+++ src/views/MenuAcao.java
@@ -2,16 +2,17 @@ package views;
 
 import controllers.AtivosController;
 import helpers.Console;
+import models.Acao;
 
 public class MenuAcao {
     
     static void exibirMenu() {
 
-        System.out.printf("\n--- ACAO ---\n");
-        System.out.println("1 - Adicionar nova acão");
-        System.out.println("2 - Remover acão");
-        System.out.println("3 - Editar acão");
-        System.out.println("4 - Buscar acão");
+        System.out.printf("\n--- AÇÃO ---\n");
+        System.out.println("1 - Adicionar nova ação");
+        System.out.println("2 - Remover ação");
+        System.out.println("3 - Editar ação");
+        System.out.println("4 - Buscar ação");
         System.out.println("5 - Adicionar transação");
         System.out.println("0 - Voltar");
 
@@ -19,38 +20,19 @@ public class MenuAcao {
 
         switch (op) {
             case 1:
-                
-                String nome = Console.lerString("Nome: ");
-                String tipoAcao = Console.lerString("Tipo de Ação: ");
-
-                try {
-                    AtivosController.cadastrarAtivo(nome, tipoAcao, false);
-                } catch (Exception e) {
-                    System.out.println(e.getMessage());
-                }
+                // adicionarAcao();
                 break;
             
             case 2:
-                nome = Console.lerString("Digite o nome do ativo que deseja excluir: ");
-                try {
-                    //AtivosController.deletarAtivo(nome);
-                } catch (Exception e) {
-                    System.out.println(e.getMessage());
-                }
+                removerAcao();
                 break;
 
             case 3:
-                // método editar
+                // editarAcao();
                 break;
             
             case 4:
-                nome = Console.lerString("Digite o nome do ativo que deseja buscar: ");
-
-                try {
-                    AtivosController.buscarAcao(nome);
-                } catch (Exception e) {
-                    System.out.println(e.getMessage());
-                }
+                // buscarAcao();
                 break;
             
             case 5:
@@ -58,9 +40,37 @@ public class MenuAcao {
                 break;
 
             default:
-                System.out.println("opção inválida! Digite Novamente");
+                System.out.println("Opção inválida! Tente Novamente...");
                 break;
         }
-
     }
+
+
+        private static void removerAcao() {
+        System.out.println("\nRemover Ação\n");
+
+        String nome = Console.lerString("Nome da Ação: ");
+
+        while (true) {
+            String confirma = Console.lerString("Deseja remover a ação " + nome + "?[S/N] ").toLowerCase();
+
+            if (confirma.equals("n")) {
+                return;
+            }
+
+            if (confirma.equals("s")) {
+                try {
+                    Acao temp = AtivosController.buscarAcao(nome);
+                    AtivosController.deletarAtivo(temp);
+                    System.out.println("Ação deletada com sucesso!");
+                } catch (Exception e) {
+                    System.out.println(e.getMessage());
+                }
+                return;
+            }
+
+            System.out.println("Opção inválida. Tente novamente...");
+        }
+    }
+
 }
